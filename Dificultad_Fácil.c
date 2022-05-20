660ab8bdae3340bc7703c40ad009694f0d0efa54
InesMelendez <80213891+InesMelendez@users.noreply.github.com>
2022-05-20 18:02:46 +0200

    ampliacion preguntas

--- Dificultad_Fácil.c
+++ Dificultad_Fácil.c
@@ -6,15 +6,19 @@ int main()
                             "2. En que siglo se escribio la Celestina?:",
                             "3. Cual es la capital de Italia?:",
                             "4. Como se llaman los triangulos con dos los lados iguales y uno desigual?:",
-                            "5. Cuantas Champions tienes el Real Madrid Club de Futbol?:"};
+                            "5. Cuantas Champions tienes el Real Madrid Club de Futbol?:",
+                            "6. Cual es el futbolista con mas balones de oro?:",
+                            "7. Cuanto duro La Guerra de los Cien Anos"};
 
    char opciones[][100] = {"A. 1492", "B. 1942", "C. 1932", "D. 1432",
                           "A. XV", "B. XII", "C. XVI", "D. XX",
                           "A. Madrid", "B. Roma", "C. Venecia", "D. Palermo",
                           "A. Escaleno", "B. Obstusangulo", "C. Equilatero", "D. Isosceles",
-                          "A. 12", "B. 13", "C. 11", "D. 14"};
+                          "A. 12", "B. 13", "C. 11", "D. 14",
+                          "A. Lionel Messi", "B. Neymar", "C. Cristiano Ronaldo", "D. Diego Maradona",
+                          "A. 100", "B. 102", "C. 116", "D. 115"};
 
-   char respuestas[] = {'A', 'A', 'B','D','B'};
+   char respuestas[] = {'A', 'A', 'B','D','B','A','C'};
    int numerodepreguntas = sizeof(preguntas)/sizeof(preguntas[0]);
 
    char adivina;
@@ -53,7 +57,7 @@ int main()
    printf("Resultado final: %d/%d\n", puntuacion, numerodepreguntas);
    printf("*********************\n");
 
-    if(puntuacion>=3)
+    if(puntuacion>=5)
 {
     printf("Has aprobado, pasa a la siguiente dificultad.\n");
 
